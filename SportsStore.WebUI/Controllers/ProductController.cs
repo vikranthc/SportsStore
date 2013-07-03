1492865035d1a993ea49f33794c5bcb8dc1d49a1
Vikranth Ceakala <vceakala@conservice.com>
2013-07-03 10:29:04 -0600

    Added ProductController/List action and a View associated with the Action

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
