49b117bb0128e41062b0cd0ea65f89c1f1ae6a2f
Vikranth Ceakala <vceakala@conservice.com>
2013-07-03 17:08:32 -0600

    Add to cart button in View and Cart model

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> _lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = _lineCollection.FirstOrDefault(p => p.Product.ProductId == product.ProductId);
+
+            if (line == null)
+            {
+                _lineCollection.Add(new CartLine { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product)
+        {
+            _lineCollection.RemoveAll(l => l.Product.ProductId == product.ProductId);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return _lineCollection.Sum(e => e.Product.Price*e.Quantity);
+        }
+
+        public void Clear()
+        {
+            _lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return _lineCollection; }
+        } 
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
