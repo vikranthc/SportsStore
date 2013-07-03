eb58f3676795fec563887e28d208cc0f64ee1bd7
Vikranth Ceakala <vceakala@conservice.com>
2013-07-03 11:19:39 -0600

    Unit Test pagination

---
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            // Arrange
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[]
+                {
+                    new Product {ProductId = 1, Name = "P1"},
+                    new Product {ProductId = 2, Name = "P2"},
+                    new Product {ProductId = 3, Name = "P3"},
+                    new Product {ProductId = 4, Name = "P4"},
+                    new Product {ProductId = 5, Name = "P5"},
+                }.AsQueryable());
+
+            var controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act
+            var result = (IEnumerable<Product>)controller.List(2).Model;
+
+            // Assert
+            var prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+    }
+}
