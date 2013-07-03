5cb7194c9b935d4c414c67948ea4b4de99db357c
Vikranth Ceakala <vceakala@conservice.com>
2013-07-03 17:58:53 -0600

    Displaying the contents of cart

---
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository _repository;
+
+        public CartController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+                {
+                    Cart = GetCart(),
+                    ReturnUrl = returnUrl
+                });
+        }
+
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = _repository.Products.FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = _repository.Products.FirstOrDefault(p => p.ProductId == productId);
+
+            if (product!=null)
+            {
+                GetCart().RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart==null)
+            {
+                cart = new Cart();
+                Session["cart"] = cart;
+            }
+
+            return cart;
+        }
+    }
+}
