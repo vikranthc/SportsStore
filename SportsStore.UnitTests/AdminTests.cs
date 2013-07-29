433a888fcab4390ccc8a411f532539a2007bab55
Vikranth <vikranth@outlook.com>
2013-07-28 20:15:48 -0600

    Implemented List View for products

---
+++ SportsStore.UnitTests/AdminTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Products()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[]
+                {
+                    new Product{ProductId = 1, Name = "P1"},
+                    new Product{ProductId = 2, Name = "P2"},
+                    new Product{ProductId = 3, Name = "P3"}
+                }.AsQueryable);
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product[] result = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual("P1", result[0].Name);
+            Assert.AreEqual("P2", result[1].Name);
+            Assert.AreEqual("P3", result[2].Name);
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository _repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+        //
+        // GET: /Admin/
+
+        public ViewResult Index()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
